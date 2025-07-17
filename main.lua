fbe78016f4c9b739fa5659640acfc3729db2865f
momoterraw <andrewvmail@gmail.com>
2025-07-17 11:59:11 -0700

    Clean up debug output and standardize sqlcipher

--- main.lua
+++ main.lua
@@ -1,84 +1,58 @@
-print "[ main.lua ] start"
-print ("[ main.lua ] user_data: " .. _G.USER_DATA_PATH )
-print ("[ main.lua ] app_resource: " .. _G.APP_RESOURCE_PATH)
+print("[ main.lua ] start")
+print("[ main.lua ] user_data: " .. _G.USER_DATA_PATH)
+print("[ main.lua ] app_resource: " .. _G.APP_RESOURCE_PATH)
 
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
-local curl = require "lcurl"
-
+local curl = require('lcurl')
 print("[ main.lua ] after require curl")
 
--- local c_struct_ca_info = curl.easy_option_by_name("cainfo")
+local vi = curl.version_info()
+print("[ curl ] version: " .. vi.version)
+print("[ curl ] host: " .. vi.host)
+print("[ curl ] ssl: " .. vi.ssl_version)
+print("[ curl ] libz: " .. vi.libz_version)
+local protocols = {}
+for k, v in pairs(vi.protocols) do
+  if v then table.insert(protocols, k) end
+end
+table.sort(protocols)
+print("[ curl ] protocols: " .. table.concat(protocols, ', '))
 
--- dump(c_struct_ca_info)
--- dump(easy)
+local feat = {}
+for k, v in pairs(vi.features) do
+  if v then table.insert(feat, k) end
+end
+print("[ curl ] features: " .. table.concat(feat, ', '))
 
--- curl.ieasy_options(curl.easy_option_by_name("cainfo"))
-
-
-print(dump(curl.version_info().version))
-print(dump(curl.version_info().host))
-print(dump(curl.version_info().features))
-print(dump(curl.OPT_CAINFO))
-print(dump(curl.OPT_CAPATH))
-
-
+local response = {}
 curl.easy{
-    url = 'https://httpbin.org/get',
-    httpheader = {
-      "X-Test-Header1: Header-Data1",
-      "X-Test-Header2: Header-Data2",
-    },
-    writefunction = print, -- use io.stderr:write()
-    [curl.OPT_VERBOSE] = true,
-    [curl.OPT_CAINFO] = _G.APP_RESOURCE_PATH .. "/cacert.pem"
-  }:perform():close()
+  url = 'https://httpbin.org/get',
+  writefunction = function(s) table.insert(response, s) end,
+  [curl.OPT_CAINFO] = _G.APP_RESOURCE_PATH .. '/cacert.pem'
+}:perform():close()
+print('[ curl ] response body:', table.concat(response))
 
-print(_G.USER_DATA_PATH .. "/test.txt", "w")
-file = io.open(_G.USER_DATA_PATH .. "/test.txt", "w")
-file:write("Hello World")
-file:close()
+local fpath = _G.USER_DATA_PATH .. '/test.txt'
+local f = assert(io.open(fpath, 'w'))
+f:write('Hello World')
+f:close()
+print(fpath .. ' written')
 
+local sqlite3 = require('lsqlite3')
+print("[ main.lua ] after require sqlite3")
 
-local sqlite3 = require("lsqlite3")
-
--- local db = sqlite3.open_memory()
-print(_G.USER_DATA_PATH .. "/test.db")
--- local db = sqlite3.open(_G.RESOURCE_PATH .. "/test.db", sqlite3.OPEN_READWRITE + sqlite3.OPEN_CREATE)
-
-local db = sqlite3.open(_G.USER_DATA_PATH .. "/test.db")
-
+local db_path = _G.USER_DATA_PATH .. '/test.db'
+print(db_path)
+local db = sqlite3.open(db_path)
 db:exec("PRAGMA key = 'test';")
-db:exec("PRAGMA cipher_plaintext_header_size = 32;")
-db:exec("PRAGMA cipher_salt = \"x'01010101010101010101010101010101'\";")
-
 db:exec[[
-  CREATE TABLE test (id INTEGER PRIMARY KEY, content);
-
-  INSERT INTO test VALUES (NULL, 'Hello World');
-  INSERT INTO test VALUES (NULL, 'Hello Lua');
-  INSERT INTO test VALUES (NULL, 'Hello Sqlite3')
+  CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, content);
+  INSERT INTO test (content) VALUES ('Hello World');
+  INSERT INTO test (content) VALUES ('Hello Lua');
+  INSERT INTO test (content) VALUES ('Hello Sqlite3');
 ]]
-
-for row in db:nrows("SELECT * FROM test") do
+for row in db:nrows('SELECT * FROM test') do
   print(row.id, row.content)
 end
+db:exec('PRAGMA cipher_version', print)
 
-local res1 = db:exec("PRAGMA cipher_version", print)
-print("res1", res1)
--- local res2 = db:exec("PRAGMA table_info(test)", print)
-
-
-
-print "[ main.lua ] end -- "
+print('[ main.lua ] end')
